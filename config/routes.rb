bcafbcf7b7318f139ecde75bb1f00e40153ceaff
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-08-04 21:07:39 +0530

    Added default route to post

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :tags
   resources :users
+
+  root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
